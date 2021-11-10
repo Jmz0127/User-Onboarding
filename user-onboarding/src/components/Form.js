ae0276350821df4613f6e388678bfc401e720b62
Jmz0127 <jmezz1@icloud.com>
2021-11-10 15:56:19 -0500

    set up form.js for name pass button and terms of service checkbox

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,93 @@
+import React from 'react'
+
+export default function Form(props) {
+const {
+    values,
+    submit,
+    change,
+    disabled,
+    errors,
+} = props
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+}
+
+const onChange = evt => {
+    const { firstName, lastName, value, checked, type } = evt.target
+    const realValue = type === 'checkbox' ? checked : value;
+    change(firstName, lastName ,realValue)
+}
+
+
+return (
+<form className='form container' onSubmit={onSubmit}>
+    <div className='form-user submit'>
+        <h2>Add a User</h2>
+
+         {/* BUTTON HERE */}
+        <button disabled={disabled}>submit</button>
+
+        {/* 🔥 RENDER THE VALIDATION ERRORS HERE - DO LATER */}
+        <div className='errors'>
+ 
+        </div>
+      </div>
+
+      <div className ='form-user inputs'>
+        <h4>General info</h4>
+
+        {/* ////////// TEXT INPUTS ////////// */}
+        {/* ////////// TEXT INPUTS ////////// */}
+        {/* ////////// TEXT INPUTS ////////// */}
+        <label>First Name
+            <input
+                value={values.firstName}
+                onChange={onChange}
+                name='firstName'
+                type='text'
+            />
+        </label>
+        <label>Last Name
+            <input
+                value={values.lastName}
+                onChange={onChange}
+                name='lastName'
+                type='text'
+            />
+        </label>
+
+
+        {/* ////////// PASSWORD ////////// */}
+        {/* ////////// PASSWORD ////////// */}
+        {/* ////////// PASSWORD ////////// */}
+        <label>Password
+            <input
+                value={values.password}
+                onChange={onChange}
+                name='password'
+                type='password'
+            />
+        </label>
+
+      </div>
+    <div className='form-user checkboxes'>
+        <h4>Terms of Service: lorem dorem ipsum</h4>
+
+        {/* ////////// CHECKBOXES ////////// */}
+        {/* ////////// CHECKBOXES ////////// */}
+        {/* ////////// CHECKBOXES ////////// */}
+        <label>I Agree
+            <input
+                type='checkbox'
+                name='agree'
+                onChange={onChange}
+                checked={values.agree}
+            />
+        </label>
+    </div>
+</form>
+)
+}
+
