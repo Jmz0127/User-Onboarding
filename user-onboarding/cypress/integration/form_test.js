e32df61fad6a5c08bdd3dfffbc77eb2b98e9e980
Jmz0127 <jmezz1@icloud.com>
2021-11-11 17:47:02 -0500

    finished MVP

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -12,6 +12,8 @@ describe('User App', () => {
     const lastNameInput = () => cy.get('input[name=lastName]');
     const emailInput = () => cy.get('input[name=email]');
     const passwordInput = () => cy.get('input[name=password]');
+    const checkTos = () => cy.get('input[type=checkbox');
+    const submitButton = () => cy.get('button');
 
 
     it('sanity check for some math', () => {
@@ -25,6 +27,13 @@ describe('User App', () => {
         expect({}).to.eql({}); // == not strict equality
     })
 
+
+
+
+    // Get the Name input and type a name in it.
+    // Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
+    // Get the Email input and type an email address in it - added assertion for personal stretch
+    // Get the password input and type a password in it - added assertion for personal stretch
     it('can type in the inputs', () => {
         firstNameInput()
             .should('have.value', '')
@@ -44,8 +53,34 @@ describe('User App', () => {
             .should('have.value', 'password');
     })
 
+    //  Set up a test that will check to see if a user can check the terms of service box
+    it('can user check the terms of service box?', () => {
+        checkTos().type('be.checked');
+        checkTos().type('be.unchecked');
+        checkTos().type('be.checked');
+    })
 
 
+    //  Check to see if a user can submit the form data
+    it('can see if a user can submit the form data', () => {
+        firstNameInput().type('Jon');
+        lastNameInput().type('Em');
+        emailInput().type('jmz111@aol.com');
+        passwordInput().type('password');
+        checkTos().type('be.checked');
+        submitButton().should('not.be.disabled');
+        submitButton().click();
+    })
+
+    //  Check to see form validation if an input is left empty
+    it('is user denied if they leave an input empty?', () => {
+        firstNameInput().should('have.value', '');
+        lastNameInput().type('Em');
+        emailInput().type('jmz111@aol.com');
+        passwordInput().type('password');
+        checkTos().type('be.checked');
+        submitButton().should('be.disabled');
+    })
     
     
 
